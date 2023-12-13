ca60257ee545e08004427a6a034e665051b7b5c2
Yoodong777 <youwoa7@daum.net>
2023-12-14 00:02:10 +0900

    Object Connect

---
+++ src/ObjectStudy/ObjectStudy_8.java
@@ -0,0 +1,8 @@
+package ObjectStudy;
+
+public class ObjectStudy_8 {
+    public static void main(String[] args) {
+        Calculator2 c1 = new Calculator2(); // ctrl 클릭 시 정의 해놓은 곳으로 이동
+        System.out.println(c1.PI);
+    }
+}
